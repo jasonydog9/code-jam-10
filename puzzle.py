18c81c456a3ef2ab9566489d045b773b67ba5b23
Inventor4life <93846658+Inventor4life@users.noreply.github.com>
2023-09-06 10:26:08 -0700

    example_main shows how to use the Puzzle class from Puzzle.py
    requirements.txt has been updated with the required dependencies.

--- puzzle.py
+++ puzzle.py
@@ -3,12 +3,10 @@ import pygame
 import PIL
 import random
 
-'''
-Parent class Puzzle
-    Description:
-        All image puzzles are a subset of this class.
-        This class splits a puzzle image up into individual pieces that can be modified, moved, and deleted.
-        It is generally assumed that the puzzle is square, so rectangular puzzles will have rectangular pieces.
+"""Parent class Puzzle
+    Description: All image puzzles are a subset of this class. This class splits a puzzle image up 
+into individual pieces that can be modified, moved, and deleted. This module assumes that puzzles will have the same 
+number or pieces tall as they are wide, so rectangular puzzles will have rectangular pieces.
 
     Inputs:
 
@@ -26,30 +24,6 @@ Parent class Puzzle
         puzzle_pos:
             this is used to determine which piece was clicked when
             the puzzle is not in the top-left corner of the window.
-
-    Methods:
-
-        modify_image(image, output_size)
-            Resizes the input image to the output size.
-            Splits the image into total_pieces parts, then creates a PuzzlePiece object from the image.
-            PuzzlePiece object is stored in a list called pieces.
-            Raises an Exception if the resized image cannot be divided into total_pieces without remainder.
-
-        get_tile_index_from_pos(mouse_position):
-            Uses puzzle_pos and mouse_position to determine which puzzle piece the mouse is currently over.
-            Returns the orderlist index of the tile
-
-        get_tile_from_pos(mouse_position):
-            Calls get_tile_index_from_pos(mouse_position)
-            returns the pieces index of the tile, which can be used to access the tile through pieces[index]
-
-        generate_orderlist():
-            re-creates the orderlist from the relative_x and relative_y values of every PuzzlePiece
-            it is a good idea to call this before using image_update()
-
-        image_update():
-            updates the puzzle image to reflect the order in orderlist. This should be called
-            at the end of every loop where the puzzle is changed
 
     Attributes:
 
@@ -77,65 +51,139 @@ Parent class Puzzle
         puzzle looks like:
         [ 1 , 0 ,
           2 , 3 ]
-'''
+"""
 
 
 class Puzzle:
-    def __init__(self, image, pieces_per_side, output_size, puzzle_pos):
+    def __init__(
+        self,
+        image: PIL.Image,
+        pieces_per_side: int,
+        output_size: tuple[int, int],
+        puzzle_pos: tuple[int, int],
+    ):
         if type(image) is not PIL.PngImagePlugin.PngImageFile:
-            raise (TypeError("Expected type " + str(PIL.PngImagePlugin.PngImageFile) + ", got type " + str(
-                type(image)) + " instead."))
+            raise (
+                TypeError(
+                    "Expected type "
+                    + str(PIL.PngImagePlugin.PngImageFile)
+                    + ", got type "
+                    + str(type(image))
+                    + " instead."
+                )
+            )
         self.output_size = output_size
         self.pieces_per_side = pieces_per_side
-        self.total_pieces = pieces_per_side ** 2
+        self.total_pieces = pieces_per_side**2
         self.image, self.shape, self.pieces = self.modify_image(image, output_size)
         self.orderlist = list(range(0, self.total_pieces))
         self.puzzle_x, self.puzzle_y = puzzle_pos
+        self.event = []
 
-    def modify_image(self, image, output_size):
+    """
+            modify_image(image, output_size)
+                Resizes the input image to the output size.
+                Splits the image into total_pieces parts, then creates a PuzzlePiece object from the image.
+                PuzzlePiece object is stored in a list called pieces.
+                Raises an Exception if the resized image cannot be divided into total_pieces without remainder.
+    """
+
+    def modify_image(self, image: PIL.Image, output_size: tuple[int, int]):
         if output_size:
             image = image.resize(output_size)
-        image = image.resize((image.size[0] - image.size[0] % self.pieces_per_side,
-                              image.size[1] - image.size[0] % self.pieces_per_side))
+        image = image.resize(
+            (
+                image.size[0] - image.size[0] % self.pieces_per_side,
+                image.size[1] - image.size[0] % self.pieces_per_side,
+            )
+        )
         self.output_size = image.size
-        self.puzzle_scale = (image.size[0] // self.pieces_per_side, image.size[1] // self.pieces_per_side)
+        self.puzzle_scale = (
+            image.size[0] // self.pieces_per_side,
+            image.size[1] // self.pieces_per_side,
+        )
         pieces = [None] * self.total_pieces
         for y in range(self.pieces_per_side):
             for x in range(self.pieces_per_side):
-                pieces[y * self.pieces_per_side + x] = np.array(image.crop((
-                    x * self.puzzle_scale[0],
-                    y * self.puzzle_scale[1],
-                    (x + 1) * self.puzzle_scale[0],
-                    (y + 1) * self.puzzle_scale[1]
-                )))
-        return_pieces = [PuzzlePiece(piece, index_relative, self) for index_relative, piece in
-                         enumerate(pieces)]
+                pieces[y * self.pieces_per_side + x] = np.array(
+                    image.crop(
+                        (
+                            x * self.puzzle_scale[0],
+                            y * self.puzzle_scale[1],
+                            (x + 1) * self.puzzle_scale[0],
+                            (y + 1) * self.puzzle_scale[1],
+                        )
+                    )
+                )
+        return_pieces = [
+            PuzzlePiece(piece, index_relative, self)
+            for index_relative, piece in enumerate(pieces)
+        ]
         return image, np.array(image).shape, return_pieces
 
-    def get_tile_index_from_pos(self, mouse_pos):
+    """
+            get_tile_index_from_pos(mouse_position):
+                Uses puzzle_pos and mouse_position to determine which puzzle piece the mouse is currently over.
+                Returns the orderlist index of the tile
+    """
+
+    def get_tile_index_from_pos(self, mouse_pos: tuple[int, int]):
         mouse_x, mouse_y = mouse_pos[0] - self.puzzle_x, mouse_pos[1] - self.puzzle_y
-        if mouse_x < 0 or mouse_y < 0 or mouse_x > self.output_size[0] or mouse_y > self.output_size[1]:
+        if (
+            mouse_x < 0
+            or mouse_y < 0
+            or mouse_x > self.output_size[0]
+            or mouse_y > self.output_size[1]
+        ):
             return None
-        tile_index = mouse_y // self.puzzle_scale[1] * self.pieces_per_side + mouse_x // self.puzzle_scale[0]
+        tile_index = (
+            mouse_y // self.puzzle_scale[1] * self.pieces_per_side
+            + mouse_x // self.puzzle_scale[0]
+        )
         return tile_index
 
-    def get_tile_from_pos(self, mouse_pos):
+    """
+            get_tile_from_pos(mouse_position):
+                Calls get_tile_index_from_pos(mouse_position)
+                returns the pieces index of the tile, which can be used to access the tile through pieces[index]
+    """
+
+    def get_tile_from_pos(self, mouse_pos: tuple[int, int]):
         tile_index = self.get_tile_index_from_pos(mouse_pos)
         if tile_index is None:
             return None
         return self.orderlist[tile_index]
 
-    def generate_orderlist(self):
+    """
+            generate_orderlist():
+                re-creates the orderlist from the relative_x and relative_y values of every PuzzlePiece
+                it is a good idea to call this before using image_update()
+    """
+
+    def generate_orderlist(self, origin_tile: int = 0):
         for i in self.pieces:
             i.relative_index = i.relative_y * self.pieces_per_side + i.relative_x
             self.orderlist[i.relative_index] = i.absolute_index
 
+    """
+            image_update():
+                updates the puzzle image to reflect the order in orderlist. This should be called
+                at the end of every loop where the puzzle is changed
+    """
+
     def image_update(self):
-        temp_array = [np.concatenate(
-            tuple([self.pieces[i].image for i in j]), axis=1) for j in
-            [self.orderlist[row * self.pieces_per_side:(row + 1) * self.pieces_per_side] for row
-             in range(self.pieces_per_side)]]
-        self.image = pygame.surfarray.make_surface(np.swapaxes(np.concatenate(tuple(temp_array), axis=0), 0, 1))
+        temp_array = [
+            np.concatenate(tuple([self.pieces[i].image for i in j]), axis=1)
+            for j in [
+                self.orderlist[
+                    row * self.pieces_per_side : (row + 1) * self.pieces_per_side
+                ]
+                for row in range(self.pieces_per_side)
+            ]
+        ]
+        self.image = pygame.surfarray.make_surface(
+            np.swapaxes(np.concatenate(tuple(temp_array), axis=0), 0, 1)
+        )
 
 
 class SlidingPuzzle(Puzzle):
@@ -147,28 +195,40 @@ class SlidingPuzzle(Puzzle):
     UPDATE = 0
     SOLVED = 1
 
-    def __init__(self, image, pieces_per_side, output_size=(), puzzle_pos=(0, 0)):
+    def __init__(
+        self,
+        image: PIL.Image,
+        pieces_per_side: int,
+        output_size: tuple[int, int] = (),
+        puzzle_pos: tuple[int, int] = (0, 0),
+    ):
         super().__init__(image, pieces_per_side, output_size, puzzle_pos)
         self.scramble()
         self.image_update()
-        self.event = []
 
     def scramble(self):
         self.orderlist.pop()
         self.last_piece = self.pieces.pop()
         self.last_image = self.last_piece.image
-        self.last_piece.image = np.full(self.pieces[0].image.shape, fill_value=0, dtype=np.uint8)
+        self.last_piece.image = np.full(
+            self.pieces[0].image.shape, fill_value=0, dtype=np.uint8
+        )
         temp_list = self.orderlist.copy()
         while temp_list == self.orderlist:
             random.shuffle(self.orderlist)
             if not self.solvable(self.orderlist):
-                self.orderlist[-1], self.orderlist[-2] = self.orderlist[-2], self.orderlist[-1]
+                self.orderlist[-1], self.orderlist[-2] = (
+                    self.orderlist[-2],
+                    self.orderlist[-1],
+                )
         self.orderlist.append(self.total_pieces - 1)
         self.pieces.append(self.last_piece)
         for pos, i in enumerate(self.orderlist):
-            self.pieces[i].relative_y, self.pieces[i].relative_x = divmod(pos, self.pieces_per_side)
+            self.pieces[i].relative_y, self.pieces[i].relative_x = divmod(
+                pos, self.pieces_per_side
+            )
 
-    def loop(self, event):
+    def loop(self, event: pygame.event):
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_start = pygame.mouse.get_pos()
             temp = self.get_tile_index_from_pos(mouse_start)
@@ -176,13 +236,20 @@ class SlidingPuzzle(Puzzle):
                 self.move(self.tile_can_move(temp))
                 self.event.append(SlidingPuzzle.UPDATE)
 
-    def tile_can_move(self, tile_index):
+    def tile_can_move(self, tile_index: int):
         # Get tiles in row
-        orderlist_index_start = tile_index // self.pieces_per_side * self.pieces_per_side
-        orderlist_index_end = tile_index // self.pieces_per_side * self.pieces_per_side + self.pieces_per_side
+        orderlist_index_start = (
+            tile_index // self.pieces_per_side * self.pieces_per_side
+        )
+        orderlist_index_end = (
+            tile_index // self.pieces_per_side * self.pieces_per_side
+            + self.pieces_per_side
+        )
         column_number = tile_index % self.pieces_per_side
-        orderlist_column = [self.orderlist[column_number + i * self.pieces_per_side] for i in
-                            range(self.pieces_per_side)]
+        orderlist_column = [
+            self.orderlist[column_number + i * self.pieces_per_side]
+            for i in range(self.pieces_per_side)
+        ]
         orderlist_row = self.orderlist[orderlist_index_start:orderlist_index_end]
         if self.total_pieces - 1 in orderlist_column:
             zero_index = orderlist_column.index(self.total_pieces - 1)
@@ -191,7 +258,10 @@ class SlidingPuzzle(Puzzle):
             if delta == 0:
                 return SlidingPuzzle.NO_MOVE, [], tile_index
             direction = SlidingPuzzle.UP if delta < 0 else SlidingPuzzle.DOWN
-            indices = [column_number + i * self.pieces_per_side for i in range(self.pieces_per_side)]
+            indices = [
+                column_number + i * self.pieces_per_side
+                for i in range(self.pieces_per_side)
+            ]
         elif self.total_pieces - 1 in orderlist_row:
             zero_index = orderlist_row.index(self.total_pieces - 1)
             tile_index_relative = tile_index % self.pieces_per_side
@@ -203,11 +273,15 @@ class SlidingPuzzle(Puzzle):
         else:
             return SlidingPuzzle.NO_MOVE, [], tile_index
 
-        movable = [indices[i] for i in
-                   range(tile_index_relative, zero_index, [i for i in direction if i != 0][0])]
+        movable = [
+            indices[i]
+            for i in range(
+                tile_index_relative, zero_index, [i for i in direction if i != 0][0]
+            )
+        ]
         return direction, movable, tile_index
 
-    def move(self, direction_tile_list_origin_tile):
+    def move(self, direction_tile_list_origin_tile: tuple[tuple[int, int], list, int]):
         direction, tile_list, origin_tile = direction_tile_list_origin_tile
         if direction == SlidingPuzzle.NO_MOVE:
             return 0
@@ -215,11 +289,12 @@ class SlidingPuzzle(Puzzle):
             self.pieces[self.orderlist[i]].relative_x += direction[0]
             self.pieces[self.orderlist[i]].relative_y += direction[1]
         self.generate_orderlist(origin_tile)
-        self.image_update()
         if self.orderlist == list(range(self.total_pieces)):
             self.event.append(SlidingPuzzle.SOLVED)
+            self.pieces[-1].image = self.last_image
+        self.image_update()
 
-    def generate_orderlist(self, origin_tile):
+    def generate_orderlist(self, origin_tile: int):
         for i in self.pieces:
             if i.absolute_index == self.total_pieces - 1:
                 i.relative_y = origin_tile // self.pieces_per_side
@@ -231,7 +306,7 @@ class SlidingPuzzle(Puzzle):
             self.orderlist[i.relative_index] = i.absolute_index
         self.orderlist[origin_tile] = self.total_pieces - 1
 
-    def solvable(self, unsorted):
+    def solvable(self, unsorted: list[int]):
         inversions = 0
         for j in range(self.total_pieces - 1):
             for i in range(j + 1, self.total_pieces - 1):
@@ -241,15 +316,20 @@ class SlidingPuzzle(Puzzle):
 
 
 class ExamplePuzzle(Puzzle):
-
-    def __init__(self, image, pieces_per_side, output_size=(), puzzle_pos=(0, 0)):
+    def __init__(
+        self,
+        image: PIL.Image,
+        pieces_per_side: int,
+        output_size: tuple[int, int] = (),
+        puzzle_pos: tuple[int, int] = (0, 0),
+    ):
         super().__init__(image, pieces_per_side, output_size, puzzle_pos)
         self.scramble()
         self.generate_orderlist()
         self.image_update()
 
     def loop(self):
-        # put code here to be run every frame, such as an event loop
+        # put code here to be run every frame, such as an event loop to detect mouse clicks
         pass
 
     def scramble(self):
@@ -258,12 +338,13 @@ class ExamplePuzzle(Puzzle):
 
 
 class PuzzlePiece:
-
-    def __init__(self, image, relative_index, master):
+    def __init__(self, image: PIL.Image, relative_index: int, master: Puzzle):
         self.image = image
         self.master = master
         self.relative_index = relative_index
         self.absolute_index = relative_index
-        self.relative_y, self.relative_x = divmod(relative_index, master.pieces_per_side)
+        self.relative_y, self.relative_x = divmod(
+            relative_index, master.pieces_per_side
+        )
         self.x = self.relative_x * master.puzzle_scale[0]
         self.y = self.relative_y * master.puzzle_scale[1]
